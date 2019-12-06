ecdccaf811721d2568edde3c89f1f0f3ad68f213
Andrew Summers <oasummerso@gmail.com>
2019-12-06 08:00:24 -0600

    Day 6 part 2.

--- lib/advent2019/day6.ex
+++ lib/advent2019/day6.ex
@@ -1,6 +1,6 @@
 defmodule Advent2019.Day6 do
   def part1(input \\ input()) do
-    graph = graph(input)
+    {graph, _} = graph(input)
 
     graph
     |> :digraph.vertices()
@@ -18,22 +18,34 @@ defmodule Advent2019.Day6 do
   end
 
   defp graph(input) do
-    Enum.reduce(input, :digraph.new(), fn line, graph ->
+    Enum.reduce(input, {:digraph.new(), :digraph.new()}, fn line, {directed_graph, undirected_graph} ->
       [from, to] =
         line
         |> String.trim()
         |> String.split(")")
 
-      :digraph.add_vertex(graph, from)
-      :digraph.add_vertex(graph, to)
-      :digraph.add_edge(graph, from, to)
+      :digraph.add_vertex(directed_graph, from)
+      :digraph.add_vertex(directed_graph, to)
+      :digraph.add_edge(directed_graph, from, to)
 
-      graph
+      :digraph.add_vertex(undirected_graph, from)
+      :digraph.add_vertex(undirected_graph, to)
+      :digraph.add_edge(undirected_graph, from, to)
+      :digraph.add_edge(undirected_graph, to, from)
+
+      {directed_graph, undirected_graph}
     end)
   end
 
   def part2(input \\ input()) do
-    0
+    {_, graph} = graph(input)
+
+    path_length =
+      graph
+      |> :digraph.get_path("YOU", "SAN")
+      |> Enum.count()
+
+    path_length - 3
   end
 
   defp input() do
