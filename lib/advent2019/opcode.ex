3e9cc552e33b9d4296adc73f461a96819fb1002b
Andrew Summers <oasummerso@gmail.com>
2019-12-07 18:08:35 -0600

    Factor processor.

--- lib/advent2019/opcode.ex
+++ lib/advent2019/opcode.ex
@@ -3,100 +3,137 @@ defmodule Advent2019.Opcode do
     process_opcode(initial, 0, input, [], false)
   end
 
-  def process_opcode(result, instruction_pointer, input, output, interrupt?) do
-    case opcode(result, instruction_pointer) do
-      [1 = _add, _param3_mode, param2_mode, param1_mode] ->
-        value_1 = value(result, instruction_pointer + 1, param1_mode)
-        value_2 = value(result, instruction_pointer + 2, param2_mode)
-        destination = Map.get(result, instruction_pointer + 3)
+  def process_opcode(state, instruction_pointer, input, output, interrupt?) do
+    [opcode, _param3_mode, param2_mode, param1_mode] = opcode(state, instruction_pointer)
 
-        result
-        |> Map.put(destination, value_1 + value_2)
+    case opcode do
+      1 ->
+        state
+        |> add(instruction_pointer, param1_mode, param2_mode)
         |> process_opcode(instruction_pointer + 4, input, output, interrupt?)
 
-      [2 = _multiply, _param3_mode, param2_mode, param1_mode] ->
-        value_1 = value(result, instruction_pointer + 1, param1_mode)
-        value_2 = value(result, instruction_pointer + 2, param2_mode)
-        destination = Map.get(result, instruction_pointer + 3)
-
-        result
-        |> Map.put(destination, value_1 * value_2)
+      2 ->
+        state
+        |> multiply(instruction_pointer, param1_mode, param2_mode)
         |> process_opcode(instruction_pointer + 4, input, output, interrupt?)
 
-      [3 = _store, _, _, _] ->
-        destination = Map.get(result, instruction_pointer + 1)
+      3 ->
         {input, remaining_input} = input(input)
 
-        result
-        |> Map.put(destination, input)
+        state
+        |> store(instruction_pointer, input)
         |> process_opcode(instruction_pointer + 2, remaining_input, output, interrupt?)
 
-      [4 = _output, _, _, param1_mode] ->
-        value = value(result, instruction_pointer + 1, param1_mode)
+      4 = _output ->
+        value = value(state, instruction_pointer + 1, param1_mode)
 
         if interrupt? do
-          {result, instruction_pointer + 2, value}
+          {state, instruction_pointer + 2, value}
         else
-          process_opcode(result, instruction_pointer + 2, input, [value | output], interrupt?)
+          process_opcode(state, instruction_pointer + 2, input, [value | output], interrupt?)
         end
 
-      [5 = _jump_if_true, _, param2_mode, param1_mode] ->
-        condition = value(result, instruction_pointer + 1, param1_mode)
+      5 ->
+        jump_pointer =
+          jump_if_true_jump_pointer(state, instruction_pointer, param1_mode, param2_mode)
 
-        if condition == 0 do
-          process_opcode(result, instruction_pointer + 3, input, output, interrupt?)
-        else
-          jump_pointer = value(result, instruction_pointer + 2, param2_mode)
-          process_opcode(result, jump_pointer, input, output, interrupt?)
-        end
+        process_opcode(state, jump_pointer, input, output, interrupt?)
 
-      [6 = _jump_if_false, _, param2_mode, param1_mode] ->
-        condition = value(result, instruction_pointer + 1, param1_mode)
+      6 ->
+        jump_pointer =
+          jump_if_false_jump_pointer(state, instruction_pointer, param1_mode, param2_mode)
 
-        if condition == 0 do
-          jump_pointer = value(result, instruction_pointer + 2, param2_mode)
-          process_opcode(result, jump_pointer, input, output, interrupt?)
-        else
-          process_opcode(result, instruction_pointer + 3, input, output, interrupt?)
-        end
+        process_opcode(state, jump_pointer, input, output, interrupt?)
 
-      [7 = _less_than, _, param2_mode, param1_mode] ->
-        value_1 = value(result, instruction_pointer + 1, param1_mode)
-        value_2 = value(result, instruction_pointer + 2, param2_mode)
-        destination = Map.get(result, instruction_pointer + 3)
-
-        value =
-          if value_1 < value_2 do
-            1
-          else
-            0
-          end
-
-        result
-        |> Map.put(destination, value)
+      7 ->
+        state
+        |> less_than(instruction_pointer, param1_mode, param2_mode)
         |> process_opcode(instruction_pointer + 4, input, output, interrupt?)
 
-      [8 = _equals, _, param2_mode, param1_mode] ->
-        value_1 = value(result, instruction_pointer + 1, param1_mode)
-        value_2 = value(result, instruction_pointer + 2, param2_mode)
-        destination = Map.get(result, instruction_pointer + 3)
-
-        value =
-          if value_1 == value_2 do
-            1
-          else
-            0
-          end
-
-        result
-        |> Map.put(destination, value)
+      8 ->
+        state
+        |> equals(instruction_pointer, param1_mode, param2_mode)
         |> process_opcode(instruction_pointer + 4, input, output, interrupt?)
 
-      [99, _, _, _] ->
-          # IO.inspect result
-
-        {Map.get(result, 0), output}
+      99 ->
+        quit(state, output)
     end
+  end
+
+  defp add(state, instruction_pointer, param1_mode, param2_mode) do
+    value_1 = value(state, instruction_pointer + 1, param1_mode)
+    value_2 = value(state, instruction_pointer + 2, param2_mode)
+    destination = Map.get(state, instruction_pointer + 3)
+
+    Map.put(state, destination, value_1 + value_2)
+  end
+
+  defp multiply(state, instruction_pointer, param1_mode, param2_mode) do
+    value_1 = value(state, instruction_pointer + 1, param1_mode)
+    value_2 = value(state, instruction_pointer + 2, param2_mode)
+    destination = Map.get(state, instruction_pointer + 3)
+
+    Map.put(state, destination, value_1 * value_2)
+  end
+
+  defp store(state, instruction_pointer, input) do
+    destination = Map.get(state, instruction_pointer + 1)
+
+    Map.put(state, destination, input)
+  end
+
+  defp jump_if_true_jump_pointer(state, instruction_pointer, param1_mode, param2_mode) do
+    condition = value(state, instruction_pointer + 1, param1_mode)
+
+    if condition == 0 do
+      instruction_pointer + 3
+    else
+      value(state, instruction_pointer + 2, param2_mode)
+    end
+  end
+
+  defp jump_if_false_jump_pointer(state, instruction_pointer, param1_mode, param2_mode) do
+    condition = value(state, instruction_pointer + 1, param1_mode)
+
+    if condition == 0 do
+      instruction_pointer + 3
+    else
+      value(state, instruction_pointer + 2, param2_mode)
+    end
+  end
+
+  defp less_than(state, instruction_pointer, param1_mode, param2_mode) do
+    value_1 = value(state, instruction_pointer + 1, param1_mode)
+    value_2 = value(state, instruction_pointer + 2, param2_mode)
+    destination = Map.get(state, instruction_pointer + 3)
+
+    value =
+      if value_1 < value_2 do
+        1
+      else
+        0
+      end
+
+    Map.put(state, destination, value)
+  end
+
+  defp equals(state, instruction_pointer, param1_mode, param2_mode) do
+    value_1 = value(state, instruction_pointer + 1, param1_mode)
+    value_2 = value(state, instruction_pointer + 2, param2_mode)
+    destination = Map.get(state, instruction_pointer + 3)
+
+    value =
+      if value_1 == value_2 do
+        1
+      else
+        0
+      end
+
+    Map.put(state, destination, value)
+  end
+
+  defp quit(state, output) do
+    {Map.get(state, 0), output}
   end
 
   defp input([first, second]) do
@@ -107,17 +144,17 @@ defmodule Advent2019.Opcode do
     {first, first}
   end
 
-  defp value(result, position, 0 = _position_mode) do
-    result[result[position]]
+  defp value(state, position, 0 = _position_mode) do
+    state[state[position]]
   end
 
-  defp value(result, position, 1 = _immediate_mode) do
-    result[position]
+  defp value(state, position, 1 = _immediate_mode) do
+    state[position]
   end
 
-  defp opcode(result, instruction_pointer) do
+  defp opcode(state, instruction_pointer) do
     [param3_mode, param2_mode, param1_mode, code1, code2] =
-      result
+      state
       |> Map.get(instruction_pointer)
       |> to_string()
       |> String.pad_leading(5, "0")
