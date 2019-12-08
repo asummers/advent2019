52d0bc3494842054a758815a016ad508bb9f1dc8
Andrew Summers <oasummerso@gmail.com>
2019-12-07 23:58:33 -0600

    Day 8 solutions

---
+++ lib/advent2019/day8.ex
@@ -0,0 +1,57 @@
+defmodule Advent2019.Day8 do
+  def part1(input \\ input(25, 6)) do
+    row =
+      Enum.min_by(input, fn chunk ->
+        row_count(chunk, 0)
+      end)
+
+    row_count(row, 1) * row_count(row, 2)
+  end
+
+  def part2(input \\ input(25, 6)) do
+    input
+    |> Enum.reverse()
+    |> Enum.reduce(List.duplicate(2, 25 * 6), fn chunk, pixels ->
+      chunk
+      |> Enum.zip(pixels)
+      |> Enum.map(fn {top, bottom} ->
+        if top == 2 do
+          bottom
+        else
+          top
+        end
+      end)
+    end)
+    |> Enum.chunk_every(25)
+    |> Enum.map_join("\n", fn chunk ->
+      Enum.map_join(chunk, "", fn n ->
+        case n do
+          0 -> " _ "
+          1 -> " X "
+        end
+      end)
+    end)
+    |> IO.puts()
+  end
+
+  defp row_count(row, m) do
+    row
+    |> Enum.filter(fn n -> n == m end)
+    |> Enum.count()
+  end
+
+  defp input(width, height) do
+    "day8.txt"
+    |> Advent2019.Utils.priv_file()
+    |> String.trim()
+    |> String.split("", trim: true)
+    |> Enum.chunk_every(width * height)
+    |> Enum.map(fn digits ->
+      Enum.map(digits, fn n ->
+        n
+        |> String.trim()
+        |> String.to_integer()
+      end)
+    end)
+  end
+end
