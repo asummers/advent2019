370623830391ff4e8abab290bc47fffb54766ebf
Andrew Summers <oasummerso@gmail.com>
2019-12-10 07:35:18 -0600

    Clean up solution with functions.

--- lib/advent2019/day10.ex
+++ lib/advent2019/day10.ex
@@ -34,25 +34,33 @@ defmodule Advent2019.Day10 do
   end
 
   defp blocked?(position, starting_position, asteroids) do
-    {position_x, position_y} = position
-    {starting_position_x, starting_position_y} = starting_position
-
-    points =
-      for x <- position_x..starting_position_x,
-          y <- position_y..starting_position_y,
-          {x, y} != position do
-        {x, y}
-      end
-      |> Enum.filter(fn {x, y} ->
-        (position_x - starting_position_x) * (position_y - y) ==
-          (position_x - x) * (position_y - starting_position_y)
-      end)
+    points = line_points(position, starting_position)
 
     asteroids
     |> Enum.reject(fn asteroid_position -> asteroid_position == position end)
     |> Enum.filter(fn point -> point in points end)
     |> Enum.empty?()
     |> Kernel.not()
+  end
+
+  defp line_points(position, starting_position) do
+    {position_x, position_y} = position
+    {starting_position_x, starting_position_y} = starting_position
+
+    for x <- position_x..starting_position_x,
+        y <- position_y..starting_position_y,
+        {x, y} != position,
+        on_line?(position, starting_position, x, y) do
+      {x, y}
+    end
+  end
+
+  defp on_line?(position, starting_position, x, y) do
+    {position_x, position_y} = position
+    {starting_position_x, starting_position_y} = starting_position
+
+    (position_x - starting_position_x) * (position_y - y) ==
+      (position_x - x) * (position_y - starting_position_y)
   end
 
   defp input() do
